27cacaf0d80973a176f1cb553efbb9fea42da339
ritsushi <s.r19990706@gmail.com>
2020-09-14 13:25:26 +0900

    Initial sharing of project

---
+++ Rectangle.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Rectagle here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Rectangle
+{
+    private int width;
+    private int height;
+    
+    public Rectangle(int width, int height){
+        this.width = width;
+        this.height = height;
+    }
+    
+    public boolean equals(){
+        return true;
+    }
+}
